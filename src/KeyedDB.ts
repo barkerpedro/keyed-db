13c6a198ddde2ad3a922c52e23e8fef852c56ae1
barkerpedro <barkerpedro@gmail.com>
2023-07-03 13:16:45 -0300

    fix log Value not found

--- src/KeyedDB.ts
+++ src/KeyedDB.ts
@@ -25,7 +25,7 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
     return this.array[0]
   }
   get last() {
-    return this.array[this.array.length-1]
+    return this.array[this.array.length - 1]
   }
   toJSON() {
     return this.array
@@ -47,7 +47,7 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
    * @returns list of updated values
    */
   upsert(...values: T[]) {
-    const updates: T[] = [] 
+    const updates: T[] = []
     values.forEach(v => {
       if (!v) return
 
@@ -68,7 +68,7 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
     values.forEach(v => {
       if (!v) return
       // if ID is present
-      const presentValue = this.get( this.idGetter(v) )
+      const presentValue = this.get(this.idGetter(v))
       if (presentValue) return
       // if key is present
       const presentKey = this.firstIndex(v)
@@ -87,23 +87,24 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
   deleteById(id: string, assertPresent: boolean = true) {
     const value = this.get(id)
     if (!value) {
-      if (assertPresent) throw new Error(`Value not found`)
+      // if (assertPresent) throw new Error(`Value not found`)
+      if (assertPresent) console.log(`Value not found`)
       else return
     }
     return this.delete(value)
   }
   delete(value: T) {
-    const index = this.firstIndex (value)
+    const index = this.firstIndex(value)
     if (index < 0 || index >= this.array.length || this.key.key(value) !== this.key.key(this.array[index])) {
-        return null
+      return null
     }
     delete this.dict[this.idGetter(value)]
-    return this.array.splice (index, 1)[0]
+    return this.array.splice(index, 1)[0]
   }
   slice(start?: number, end?: number) {
-    const db = new KeyedDB (this.key, this.idGetter)
-    db.array = this.array.slice (start, end)
-    db.array.forEach (item => db.dict[ this.idGetter(item) ] = item)
+    const db = new KeyedDB(this.key, this.idGetter)
+    db.array = this.array.slice(start, end)
+    db.array.forEach(item => db.dict[this.idGetter(item)] = item)
     return db
   }
   /** Clears the DB */
@@ -123,7 +124,7 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
    * @param id 
    * @param update 
    */
-  update(id: string, update: (value: T) => void) {    
+  update(id: string, update: (value: T) => void) {
     const value = this.get(id)
     if (value) {
       const idx = this.firstIndex(value)
@@ -149,10 +150,10 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
     return this.update(this.idGetter(value), update)
   }
   filter(predicate: (value: T, index: number) => boolean) {
-    const db = new KeyedDB (this.key, this.idGetter)
-    db.array = this.array.filter ((value, index) => {
+    const db = new KeyedDB(this.key, this.idGetter)
+    db.array = this.array.filter((value, index) => {
       if (predicate(value, index)) {
-        db.dict[ this.idGetter (value) ] = value
+        db.dict[this.idGetter(value)] = value
         return true
       }
     })
@@ -166,7 +167,7 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
    * @param mode whether to get the content `before` the cursor or `after` the cursor; default=`after`
    */
   paginatedByValue(value: T | null, limit: number, predicate?: (value: T, index: number) => boolean, mode: PaginationMode = 'after') {
-    return this.paginated (value && this.key.key(value), limit, predicate, mode)
+    return this.paginated(value && this.key.key(value), limit, predicate, mode)
   }
   /**
    * Get the values of the data in a paginated manner
@@ -178,59 +179,59 @@ export default class KeyedDB<T, K> implements IKeyedDB<T, K> {
   paginated(cursor: K | null, limit: number, predicate?: (value: T, index: number) => boolean, mode: PaginationMode = 'after') {
     let index = mode === 'after' ? 0 : this.array.length
     if (cursor !== null && typeof cursor !== 'undefined') {
-      index = binarySearch (this.array, v => this.key.compare(cursor, this.key.key(v)))
-    
+      index = binarySearch(this.array, v => this.key.compare(cursor, this.key.key(v)))
+
       if (index < 0) index = 0
       if (this.key.key(this.array[index]) === cursor) index += (mode === 'after' ? 1 : 0)
     }
-    return this.filtered (index, limit, mode, predicate)
+    return this.filtered(index, limit, mode, predicate)
   }
   private _insertSingle(value: T) {
-    if (!value) throw new Error ('falsey value')
+    if (!value) throw new Error('falsey value')
 
     const valueID = this.idGetter(value)
     if (this.get(valueID)) {
-        throw new Error('duplicate ID being inserted: ' + valueID)
+      throw new Error('duplicate ID being inserted: ' + valueID)
     }
 
     if (this.array.length > 0) {
-      const index = this.firstIndex (value)
-      
-      if (index >= this.array.length) this.array.push (value)
-      else if (index < 0) this.array.unshift (value)
-      else if (this.key.key(value) !== this.key.key(this.array[index])) this.array.splice (index, 0, value)
+      const index = this.firstIndex(value)
+
+      if (index >= this.array.length) this.array.push(value)
+      else if (index < 0) this.array.unshift(value)
+      else if (this.key.key(value) !== this.key.key(this.array[index])) this.array.splice(index, 0, value)
       else throw new Error(`duplicate key: ${this.key.key(value)}, of inserting: ${valueID}, present: ${this.idGetter(this.array[index])}`)
-    
+
     } else {
-      this.array.push (value)
-    }  
+      this.array.push(value)
+    }
     this.dict[valueID] = value
   }
-  private filtered (start: number, count: number, mode: PaginationMode, predicate?: (value: T, index: number) => boolean) {
+  private filtered(start: number, count: number, mode: PaginationMode, predicate?: (value: T, index: number) => boolean) {
     let arr: T[]
     if (mode === 'after') {
       if (predicate) {
         arr = []
-        for (let item of this.array.slice (start)) {
-          predicate (item, start+arr.length) && arr.push (item)
+        for (let item of this.array.slice(start)) {
+          predicate(item, start + arr.length) && arr.push(item)
           if (arr.length >= count) break
         }
-      } else arr = this.array.slice (start, start+count)
+      } else arr = this.array.slice(start, start + count)
     } else if (mode === 'before') {
       if (predicate) {
         arr = []
-        for (let i = start-1; i >= 0; i--) {
+        for (let i = start - 1; i >= 0; i--) {
           let item = this.array[i]
-          predicate (item, start+arr.length) && arr.unshift (item)
+          predicate(item, start + arr.length) && arr.unshift(item)
           if (arr.length >= count) break
         }
-      } else arr = this.array.slice ( Math.max(start-count, 0), start)
+      } else arr = this.array.slice(Math.max(start - count, 0), start)
     }
     return arr
   }
-  private firstIndex (value: T) {
+  private firstIndex(value: T) {
     const valueKey = this.key.key(value)
-    return binarySearch (this.array, v => this.key.compare(valueKey, this.key.key(v)))
+    return binarySearch(this.array, v => this.key.compare(valueKey, this.key.key(v)))
   }
 
   *[Symbol.iterator](): Iterator<T> {
